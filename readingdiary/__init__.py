5edc96cf9e21375f82d1dad0fbde0575f86ce65d
miguelykfr-afk <miguelykfr@gmail.com>
2026-03-09 17:51:04 -0500

    segundo

--- readingdiary/__init__.py
+++ readingdiary/__init__.py
@@ -12,3 +12,15 @@ class Note:
 
 
 class Book:
+    EXCELLENT = 3
+    GOOD = 2
+    BAD = 1
+    UNRATED = -1
+
+    def __init__(self, isbn, title, author, pages):
+        self.isbn = isbn
+        self.title = title
+        self.author = author
+        self.pages = pages
+        self.rating = Book.UNRATED
+        self.notes = []
